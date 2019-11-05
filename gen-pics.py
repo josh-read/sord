92660ecf0d2608b64b86a02c08f00409a1f30e99
Joshua Read <josh@pop-os.localdomain>
2019-11-05 14:41:00 +0000

    Loop through generation

--- gen-pics.py
+++ gen-pics.py
@@ -2,6 +2,11 @@ import numpy as np
 from PIL import Image
 from random import randint, choice
 from string import ascii_letters as chars
+import os
+import shutil
+
+
+TARGET_DIR = 'mixed-pics'
 
 
 def random_color():
@@ -34,11 +39,29 @@ def random_image():
     img_name = ''.join(img_name)
     ext = ['jpg', 'png', 'pdf']
     img_name = '.'.join([img_name, choice(ext)])
-    img.save(img_name)
+    img.save('/'.join([TARGET_DIR, img_name]))
+
+
+def clear_folder():
+    files = os.listdir(TARGET_DIR)
+    filepaths = (os.path.join(TARGET_DIR, f) for f in files)
+    for p in filepaths:
+        os.remove(p)
+
+
+def duplicate_images():
+    files = os.listdir(TARGET_DIR)
+    for i, f in enumerate(files):
+        path = os.path.join(TARGET_DIR, f)
+        if (i%2 == 0):
+            shutil.copyfile(path, os.path.join(TARGET_DIR, ('copy'+f)))
 
 
 def main():
-    random_image()
+    clear_folder()
+    [random_image() for _ in range(10)]
+    duplicate_images()
+
 
 if __name__ == '__main__':
     main()
