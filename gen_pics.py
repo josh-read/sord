66e9ac9fda0eeecea4a203b3453c8bdb968ea461
jon <jon@jons-MacBook-Pro.local>
2021-10-03 19:59:28 +0100

    Add docstrings to all functions in gen_pics.py

--- gen_pics.py
+++ gen_pics.py
@@ -5,10 +5,12 @@ import os
 import shutil
 
 
+# Path to directory in which images will be generated
 TARGET_DIR = 'mixed_pics'
 
 
 def random_color():
+    """Generate a random 8 bit RGB color."""
     red = randint(0, 255)
     green = randint(0, 255)
     blue = randint(0, 255)
@@ -30,6 +32,7 @@ def random_image():
 
 
 def clear_folder():
+    """Remove all files in the target directory."""
     files = os.listdir(TARGET_DIR)
     filepaths = (os.path.join(TARGET_DIR, f) for f in files)
     for p in filepaths:
@@ -37,6 +40,7 @@ def clear_folder():
 
 
 def duplicate_images():
+    """Copy half of the files in target directory, prefixing them with 'copy'."""
     files = os.listdir(TARGET_DIR)
     for i, f in enumerate(files):
         path = os.path.join(TARGET_DIR, f)
@@ -45,6 +49,7 @@ def duplicate_images():
 
 
 def main():
+    """Empty the target directory, generate 10 new random images and copy 5 of them."""
     clear_folder()
     [random_image() for _ in range(10)]
     duplicate_images()
